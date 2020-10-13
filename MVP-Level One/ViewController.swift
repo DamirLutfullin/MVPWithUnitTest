b1675ef0ede1be6efea7c6b392492fe8af610db0
Damir Lutfullin <dmr.lutfullin@gmail.com>
2020-10-13 14:38:27 +0500

    Initial Commit

---
+++ MVP-Level One/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP-Level One
+//
+//  Created by Damir Lutfullin on 13.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
